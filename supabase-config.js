e17092fa8e6e90f92483302d01d025fda759bb11
EthanDBSV <ethantruong29@gmail.com>
2026-09-05 23:21:16 -0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-﻿window.DRAFTDEX_SUPABASE = {
-  url: 'https://obgxhjvvnpbjocuylump.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iZ3hoanZ2bnBiam9jdXlsdW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwMzI3NzcsImV4cCI6MjEwMzYwODc3N30.2I20kfViYwtt84T8FW0isTRUMRrHi2bBRolUbFlX2qA'
-};
